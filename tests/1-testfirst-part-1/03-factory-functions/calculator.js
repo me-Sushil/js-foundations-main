9c7ceac2315d6561710217747fffeb953fb637d4
me-Sushil <girbahadurbk123@gmail.com>
2025-07-16 18:43:47 +0545

    factory function calculater commit

--- tests/1-testfirst-part-1/03-factory-functions/calculator.js
+++ tests/1-testfirst-part-1/03-factory-functions/calculator.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+function createCalculator() {
+  return {
+    total: 0,
+    add(num) {
+      this.total += num;
+    },
+    subtract(num) {
+      this.total -= num;
+    },
+    value(){
+        return this.total;
+    },
+    clear(){
+        this.total = 0;
+    }
+  };
+};
