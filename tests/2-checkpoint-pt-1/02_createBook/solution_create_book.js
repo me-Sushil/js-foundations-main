d870b304bcf2118c81fa7c96b6d90c0519cf8815
me-Sushil <girbahadurbk123@gmail.com>
2025-07-18 00:29:34 +0545

    create book using prototype, and object commit

--- tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
+++ tests/2-checkpoint-pt-1/02_createBook/solution_create_book.js
@@ -1,1 +1,32 @@
-/* eslint-disable no-unused-vars, no-prototype-builtins */
+// /* eslint-disable no-unused-vars, no-prototype-builtins */
+// Shared prototype with all methods
+const bookPrototype = {
+  getInfo() {
+    return `${this.title} by ${this.author}`;
+  },
+  getPrice() {
+    return this.price;
+  },
+  addRating(stars) {
+    this.rating.push(stars);
+  },
+  getRating() {
+    if (this.rating.length === 0) return 0;
+    let total = 0;
+    for (let starStr of this.rating) {
+      total += starStr.length;
+    }
+    return total / this.rating.length;
+  }
+};
+
+// Factory function to create a book instance
+function createBook(id, title, author, price) {
+  const book = Object.create(bookPrototype);
+  book.id = id;
+  book.title = title;
+  book.author = author;
+  book.price = price;
+  book.rating = [];
+  return book;
+}
