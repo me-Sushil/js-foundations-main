82f7a473eb7ba11a30025ff0e0d1b4f60840c1ff
me-Sushil <girbahadurbk123@gmail.com>
2025-07-17 23:48:39 +0545

    create dynamic matrix commit

--- tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
+++ tests/2-checkpoint-pt-1/01_multiplication_table/solution_multiplication_table.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+const multiplicationTable =(rows, columns)=>{
+    let newArr = [];
+    for(let i=1; i<=rows; i++){
+        let row = [];
+        for(let j=1; j<=columns; j++){
+            row.push(i*j);
+        }
+        newArr.push(row);
+    }
+    return newArr;
+}
+
+const arr = multiplicationTable(2,3);
+console.log(arr);
