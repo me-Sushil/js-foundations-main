49829337c856baf378d4016e230eafd08b785948
me-Sushil <girbahadurbk123@gmail.com>
2025-07-17 21:02:34 +0545

    sort through loop lower to higher commit

--- tests/1-testfirst-part-1/04-loops/loops.js
+++ tests/1-testfirst-part-1/04-loops/loops.js
@@ -64,3 +64,20 @@ const paramifyObjectKeys =(obj)=>{
     .sort()
     .join('&');
 };
+
+function sort(items) {
+  let length = items.length;
+  for (let i = length - 1; i >= 0; i--) {
+    //Number of passes
+    for (let j = length - i; j > 0; j--) {
+      //Compare the adjacent positions
+      if (items[j] < items[j - 1]) {
+        //Swap the numbers
+        let tmp = items[j];
+        items[j] = items[j - 1];
+        items[j - 1] = tmp;
+      }
+    }
+  }
+  return items;
+}
