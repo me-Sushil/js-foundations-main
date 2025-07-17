e2c1d17d7beb63761094cca840219c4bee798402
me-Sushil <girbahadurbk123@gmail.com>
2025-07-17 21:51:55 +0545

    for in loop in key and check hasOwnProperty and join them commit

--- tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
+++ tests/2-checkpoint-pt-1/00_find_obj_properties/solution_find_obj_properties.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function findObjPropsHasOwn(obj) {
+  const result = [];
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      result.push(key);
+    }
+  }
+
+  return result.join(', ');
+}
