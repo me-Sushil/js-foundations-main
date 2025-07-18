423059347f5a0511adf84310ef48827902d32670
me-Sushil <girbahadurbk123@gmail.com>
2025-07-18 17:43:38 +0545

    map using loop and callback concept solve

--- tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
+++ tests/3-testfirst-part-2/07-functional-programming/00-recreate-array-methods/functional.js
@@ -1,7 +1,19 @@
 /* eslint-disable no-unused-vars */
 
-function forEach(arr){
-    arr.forEach((element)=>{
-        console.log(element);
-    })
+function forEach(arr) {
+  arr.forEach((element) => {
+    console.log(element);
+  });
+}
+
+function map(arr, doubler) {
+    let newArr = [];
+    for(let i=0; i<arr.length; i++){
+        let element = arr[i];
+        newArr.push(doubler(element));
+    }
+     return newArr;
+}
+ function doubler (num){
+    return num *2;
 }
