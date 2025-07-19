0f792df24adc14c1c5c36e8acd2ddf2dcc133fb4
me-Sushil <girbahadurbk123@gmail.com>
2025-07-19 22:41:56 +0545

    array of supperhero object into single object solve

--- tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
+++ tests/3-testfirst-part-2/07-functional-programming/01-use-array-methods/use-array-methods.js
@@ -44,3 +44,17 @@ function getPopulation(countriesArray, selectedCountryNames) {
     return total;
   }, 0);
 }
+
+
+function keyifyArrayOfObjects(key, array) {
+  return array.reduce((result, obj) => {
+    result[obj[key]] = obj;
+    return result;
+  }, {});
+}
+
+// function powerLevelAverage(superheroes) {
+//   const total = superheroes.reduce((sum, hero) => sum + hero.powerLevel, 0);
+//   return Math.round(total / superheroes.length);
+// }
+
