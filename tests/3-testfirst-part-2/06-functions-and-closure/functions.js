0c7e18eaee8b69dced264eefb6756909e966154d
me-Sushil <girbahadurbk123@gmail.com>
2025-07-19 13:35:43 +0545

    has been called or not , run once solve

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -1,25 +1,32 @@
 /* eslint-disable no-unused-vars */
 function concatString() {
-  const arr = Array.from(arguments); 
-  return arr.join('');              
+  const arr = Array.from(arguments);
+  return arr.join("");
 }
 
-
-
-
-function yourFunctionRunner(){
-  let str = '';
-  let valArr = Array.from(arguments); 
-  for(let i =0; i<valArr.length; i++){
+function yourFunctionRunner() {
+  let str = "";
+  let valArr = Array.from(arguments);
+  for (let i = 0; i < valArr.length; i++) {
     let element = valArr[i];
-    str+=element();
+    str += element();
   }
   return str;
 }
 
+const makeAdder = (num) => {
+  return function (numm) {
+    return num + numm;
+  };
+};
 
-const makeAdder = (num)=>{
-  return function(numm){
-    return num+numm;
-  }
-}
+const once = (sayHello) => {
+  let hasBeenCalled = false;
+  return function () {
+    if (!hasBeenCalled) {
+      hasBeenCalled = true;
+      return sayHello();
+    }
+    return "the function has already been called...";
+  };
+};
