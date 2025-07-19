bf86d9819804a046481f0e951e1f57cba54b111c
me-Sushil <girbahadurbk123@gmail.com>
2025-07-19 12:38:23 +0545

    callback, array.from and concatinate solve

--- tests/3-testfirst-part-2/06-functions-and-closure/functions.js
+++ tests/3-testfirst-part-2/06-functions-and-closure/functions.js
@@ -3,3 +3,17 @@ function concatString() {
   const arr = Array.from(arguments); 
   return arr.join('');              
 }
+
+
+
+
+function yourFunctionRunner(){
+  let str = '';
+  let valArr = Array.from(arguments); 
+  for(let i =0; i<valArr.length; i++){
+    let element = valArr[i];
+    str+=element();
+  }
+  return str;
+}
+
