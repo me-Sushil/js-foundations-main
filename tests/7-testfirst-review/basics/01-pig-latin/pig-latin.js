79733152e9954ac44ac52f7cbcf070a17d343322
me-Sushil <girbahadurbk123@gmail.com>
2025-07-21 03:11:13 +0545

    pig latin solved

--- tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
+++ tests/7-testfirst-review/basics/01-pig-latin/pig-latin.js
@@ -1,1 +1,38 @@
 /* eslint-disable no-unused-vars */
+const pigify = (string) => {
+  const isVowel = (char) => "aeiou".includes(char);
+
+  const convertWord = (word) => {
+    word = word.toLowerCase();
+
+    if (isVowel(word[0])) {
+      return word + "ay";
+    }
+
+    // Handle special cases for 'qu' and 'sch'
+    let consonantCluster = "";
+    let i = 0;
+
+    while (i < word.length) {
+      if (word.slice(i, i + 2) === "qu") {
+        consonantCluster += "qu";
+        i += 2;
+      } else if (word.slice(i, i + 3) === "sch") {
+        consonantCluster += "sch";
+        i += 3;
+      } else if (!isVowel(word[i])) {
+        consonantCluster += word[i];
+        i++;
+      } else {
+        break;
+      }
+    }
+
+    return word.slice(i) + consonantCluster + "ay";
+  };
+
+  return string
+    .split(" ")
+    .map(convertWord)
+    .join(" ");
+};
