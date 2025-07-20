6c0e6ceab558c0fec3ba9d3143a79a28ae5efc22
me-Sushil <girbahadurbk123@gmail.com>
2025-07-20 20:54:34 +0545

    class constructor method solved

--- tests/6-testfirst-part-3/08-Classes/01-classes/classes.js
+++ tests/6-testfirst-part-3/08-Classes/01-classes/classes.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+
+class Mammal {
+  constructor(name) {
+    this.name = name;
+    this.offspring = [];
+  }
+  sayHello() {
+    return `My name is ${this.name}, I'm a Mammal`;
+  }
+
+  haveBaby() {
+    const baby = new Mammal(`Baby ${this.name}`);
+    this.offspring.push(baby);
+    return baby;
+  }
+}
