c1663209596d09aa93331274f0208f4c5ab913f2
me-Sushil <girbahadurbk123@gmail.com>
2025-07-20 20:25:54 +0545

    Calculator using reverse polish notation solved

--- tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
+++ tests/6-testfirst-part-3/08-Classes/00-refactor-RPN-Calculator/solution-refactor-rpn-calculator.js
@@ -3,3 +3,50 @@
 /*
   Refactor the constructor function below and use ES2015 Class Syntax
 */
+class RPNCalculator {
+  constructor() {
+    this.stack = [];
+  }
+
+  push(val) {
+    this.stack.push(val);
+  }
+
+  _requireOperands() {
+    if (this.stack.length < 2) {
+      throw new Error("rpnCalculatorInstance is empty");
+    }
+  }
+
+  plus() {
+    this._requireOperands();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a + b);
+  }
+
+  minus() {
+    this._requireOperands();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a - b);
+  }
+
+  divide() {
+    this._requireOperands();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a / b);
+  }
+
+  times() {
+    this._requireOperands();
+    const b = this.stack.pop();
+    const a = this.stack.pop();
+    this.stack.push(a * b);
+  }
+
+  value() {
+    return this.stack[this.stack.length - 1];
+  }
+}
