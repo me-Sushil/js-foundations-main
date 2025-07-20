716c00fa5ebbb710f0c11f99099d90daa724063d
me-Sushil <girbahadurbk123@gmail.com>
2025-07-20 16:53:35 +0545

    clouser and callback concept solved

--- tests/5-checkpoint-pt-2/03_thrice/solution_thrice.js
+++ tests/5-checkpoint-pt-2/03_thrice/solution_thrice.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars */
+const thrice = (fn) => {
+  let count = 0;
+  return function() {
+    if (count < 3) {
+      count++;
+      return fn();  // properly calls the original function
+    } else {
+      return undefined;
+    }
+  };
+};
