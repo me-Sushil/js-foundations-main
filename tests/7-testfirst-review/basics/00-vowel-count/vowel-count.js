dc81f7b7e3ca511b44c19b115fedd548f1681401
me-Sushil <girbahadurbk123@gmail.com>
2025-07-21 02:13:05 +0545

    vowel count solved

--- tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
+++ tests/7-testfirst-review/basics/00-vowel-count/vowel-count.js
@@ -1,1 +1,23 @@
 /* eslint-disable no-unused-vars */
+const vowelsCount =(string)=>{
+    let vowelCount = {
+      a: 0,
+      e: 0,
+      i: 0,
+      o: 0,
+      u: 0,
+      total: 0,
+    };
+   let arrOfStr = string.toLowerCase().split("");
+   for(let key of arrOfStr){
+    if(vowelCount.hasOwnProperty(key)){
+        vowelCount[key] += 1;
+    }
+   }
+vowelCount.total = Object.entries(vowelCount).reduce((acc, [key, value]) => {
+    return key !== "total" ? acc + value : acc;
+  }, 0);
+
+  return vowelCount;
+};
+   
