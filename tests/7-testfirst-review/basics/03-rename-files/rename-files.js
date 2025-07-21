31f734c6160ed7a96b6fa4d86c3965ade60944f6
me-Sushil <girbahadurbk123@gmail.com>
2025-07-21 18:18:31 +0545

    rename files solved

--- tests/7-testfirst-review/basics/03-rename-files/rename-files.js
+++ tests/7-testfirst-review/basics/03-rename-files/rename-files.js
@@ -1,1 +1,26 @@
-/* eslint-disable no-unused-vars */
+function renameFiles(names) {
+    const nameCount = {}; 
+    const result = [];
+
+    for (let name of names) {
+        if (nameCount[name] === undefined) {
+            nameCount[name] = 1;
+            result.push(name);
+        } else {
+            let newName;
+            let count = nameCount[name];
+
+            while (nameCount[`${name}(${count})`] !== undefined) {
+                count++;
+            }
+
+            newName = `${name}(${count})`;
+            result.push(newName);
+
+            nameCount[name]++;
+            nameCount[newName] = 1;
+        }
+    }
+
+    return result;
+}
