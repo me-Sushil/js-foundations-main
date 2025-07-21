28fb22e7f288ffc80c775a9fec1fdb8f91fc86f0
me-Sushil <girbahadurbk123@gmail.com>
2025-07-21 18:59:54 +0545

    map quest using reduce

--- tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
+++ tests/7-testfirst-review/basics/04-map-quest/solution-map-quest.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+const mapQuest =(array)=>{
+    let oppositeDir = {
+    'N': 'S',
+    'S': 'N',
+    'E': 'W',
+    'W': 'E'
+    }
+   return array.reduce((stack, dir)=>{
+        if(stack.length > 0 && oppositeDir[dir] === stack[stack.length-1]){
+            stack.pop();
+        }else{
+            stack.push(dir);
+        }
+        return stack;
+    },[])
+
+}
