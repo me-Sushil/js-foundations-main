0dbf320b0c8a923ccb929160554734dfa48209a6
me-Sushil <girbahadurbk123@gmail.com>
2025-07-21 19:21:36 +0545

    multiple maps solved

--- tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
+++ tests/7-testfirst-review/basics/05-multiple-maps/multiple-maps.js
@@ -1,1 +1,15 @@
 /* eslint-disable no-unused-vars */
+// the values from the first array argument are mapped to the array of objects in the second array argument
+// properly maps values from the first array argument when there are multiple maps in the second argument
+
+
+function multipleMaps(mapper, maps) {
+  return mapper.map((value) => {
+    for (let map of maps) {
+      if (Object.prototype.hasOwnProperty.call(map, value)) {
+        return map[value];
+      }
+    }
+    return undefined; 
+  });
+}
