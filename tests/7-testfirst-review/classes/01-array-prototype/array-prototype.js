66b9ede5291c30af7454de61c9e2709f76f8504e
me-Sushil <girbahadurbk123@gmail.com>
2025-07-23 00:21:39 +0545

    custom filter solved

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -1,4 +1,4 @@
-Array.prototype.maap = function(callback) {
+Array.prototype.maap = function (callback) {
   const result = [];
   this.forEach((element) => {
     result.push(callback(element));
@@ -6,8 +6,12 @@ Array.prototype.maap = function(callback) {
   return result;
 };
 
-
-
-
-
-
+Array.prototype.fiilter = function (callback) {
+  let result = [];
+  this.forEach((element) => {
+    if (callback(element)) {
+      result.push(element);
+    }
+  });
+  return result;
+};
