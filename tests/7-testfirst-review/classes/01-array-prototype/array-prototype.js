3e3284631c2ceb344e85cea4af29b0491e1b7f95
me-Sushil <girbahadurbk123@gmail.com>
2025-07-22 22:44:06 +0545

    create map solved

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -1,1 +1,17 @@
-/* eslint-disable no-unused-vars */
+Array.prototype.maap = function(callback) {
+  const result = [];
+  this.forEach((element) => {//, index, array
+    result.push(callback(element));//, index, array
+  });
+  return result;
+};
+const arr = [1, 2, 3];
+const newArr = arr.maap((val, i) => {
+  return val + 1;
+});
+console.log(newArr); 
+console.log(arr);  
+
+
+
+
