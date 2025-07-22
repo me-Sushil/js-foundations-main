2fa42f0467200d25f45ed0309a2be8b59bac04f6
me-Sushil <girbahadurbk123@gmail.com>
2025-07-22 22:47:58 +0545

    update

--- tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
+++ tests/7-testfirst-review/classes/01-array-prototype/array-prototype.js
@@ -1,6 +1,6 @@
 Array.prototype.maap = function(callback) {
   const result = [];
-  this.forEach((element) => {//, index, array
+  this.forEach((element) => {
     result.push(callback(element));//, index, array
   });
   return result;
