740bc3006e2a87320bb076bb1ed1f2a016f50b6d
me-Sushil <girbahadurbk123@gmail.com>
2025-07-22 21:31:01 +0545

    class extends and super solved

--- tests/7-testfirst-review/classes/00-vehicles/vehicle.js
+++ tests/7-testfirst-review/classes/00-vehicles/vehicle.js
@@ -1,1 +1,31 @@
-/* eslint-disable no-unused-vars */
+// /* eslint-disable no-unused-vars */
+class Vehicle{
+    constructor ([wheelTotal, energySource, manufacturer, isOn = false]){
+   // constructor (array){
+    this.wheelTotal = wheelTotal,
+    this.energySource = energySource,
+    this.manufacturer = manufacturer,
+    this.isOn = isOn
+    }
+   getSpec(key) {
+//   console.log('Looking for:', key, 'Found:', this[key]);
+  return this[key];
+ }
+}
+
+class Tesla extends Vehicle {
+     constructor([milesPerCharge, model, chargePercentage, minutesToCharge, [vehicleData]]) {
+    super(vehicleData); 
+    this.milesPerCharge = milesPerCharge;
+    this.model = model;
+    this.chargePercentage = chargePercentage;
+    this.minutesToCharge = minutesToCharge;
+    }
+
+   minutesToFullCharge(chargePer) {
+  const minutesLeft = (100 - chargePer);
+  return `${minutesLeft} minutes until the charge is full!`;
+}
+
+}
+
