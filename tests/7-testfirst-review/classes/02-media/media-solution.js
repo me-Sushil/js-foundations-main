58300726a2eba09513a51efff63d802de1c01a47
me-Sushil <girbahadurbk123@gmail.com>
2025-07-23 11:08:09 +0545

    media solution solved

--- tests/7-testfirst-review/classes/02-media/media-solution.js
+++ tests/7-testfirst-review/classes/02-media/media-solution.js
@@ -1,1 +1,30 @@
 /* eslint-disable no-unused-vars */
+class Media {
+  constructor(title, yearOfRelease) {
+    (this.title = title),
+    (this.yearOfRelease = yearOfRelease),
+    ( this.ratings = []);
+  }
+  getTitle() {
+    return this.title;
+  }
+  getYearOfRelease() {
+    return this.yearOfRelease;
+  }
+  addRating(rating) {
+    if (rating < 1 || rating > 100) {
+    `${rating} is not a valid rating, ratings must be 1-100`;
+    } else{
+         this.ratings.push(rating);
+    }
+  }
+  getAverageRating() {
+    if (this.ratings.length === 0) {
+      return `this is not rated`;
+    } 
+     let total = this.ratings.reduce((acc, currentValue) => acc + currentValue ,0);
+    return Math.floor(total / this.ratings.length); // Use floor for integer average
+
+    
+  }
+}
