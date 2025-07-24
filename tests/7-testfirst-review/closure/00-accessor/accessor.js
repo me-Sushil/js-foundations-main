3bc76fa0cc19e9236426811948f14864e785c7fb
me-Sushil <girbahadurbk123@gmail.com>
2025-07-24 23:26:38 +0545

    accessor solved using obj key value

--- tests/7-testfirst-review/closure/00-accessor/accessor.js
+++ tests/7-testfirst-review/closure/00-accessor/accessor.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-unused-vars */
+function accessor(obj) {
+  return function (key, value) {
+    if (arguments.length === 1) {
+      return obj[key];
+    } else if(arguments.length === 2){
+        obj[key] = value;
+    }
+
+  };
+}
