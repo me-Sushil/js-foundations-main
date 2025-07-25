2b0f204e3d930f14abde216858747b906207c7f8
me-Sushil <girbahadurbk123@gmail.com>
2025-07-25 10:41:54 +0545

    call count solved

--- tests/7-testfirst-review/closure/01-sometimes/sometimes.js
+++ tests/7-testfirst-review/closure/01-sometimes/sometimes.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+function sometimes(func) {
+ let callCount = 0;
+    return function (...args){
+        callCount++;
+
+        if(callCount <=3){
+            return func(...args);
+        }
+        else if(callCount % 2 === 0){
+            return "I do not know!";
+        }
+        else {
+            return func(...args);
+        }
+    }
+}
