98a88cc1614346d5776544b91b28b32bd0d5d474
me-Sushil <girbahadurbk123@gmail.com>
2025-07-25 16:14:48 +0545

    make iterator solved

--- tests/7-testfirst-review/closure/02-make-iterator/make-iterator.js
+++ tests/7-testfirst-review/closure/02-make-iterator/make-iterator.js
@@ -1,1 +1,19 @@
 /* eslint-disable no-unused-vars */
+function makeIterator(array){
+    let index = 0;
+    return {
+        getNext : function(){
+        if(index < array.length){
+            let result = {value : array[index], done : false};
+            index++;
+            return result;
+        }else{
+            return {value : undefined, done : true};
+        }
+        },
+
+        getIndex : function(){
+            return index;
+        }
+    }
+}
