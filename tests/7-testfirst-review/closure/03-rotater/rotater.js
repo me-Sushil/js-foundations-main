1d7f0e4e1d346604a58e77624422d0e2ce6b067f
me-Sushil <girbahadurbk123@gmail.com>
2025-07-25 21:11:32 +0545

    rotater solved

--- tests/7-testfirst-review/closure/03-rotater/rotater.js
+++ tests/7-testfirst-review/closure/03-rotater/rotater.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars */
+function rotater(string) {
+  let direction = 'forward';
+  return function (num) {
+    const len = string.length;
+    const n = num % len;
+    let rotated;
+    if (direction === 'forward') {
+      rotated = string.slice(n) + string.slice(0, n);
+    } else {
+      rotated = string.slice(-n) + string.slice(0, -n);
+    }
+    if (num === len) {
+      direction = direction === 'forward' ? 'backward' : 'forward';
+    }
+    return rotated;
+  };
+}
