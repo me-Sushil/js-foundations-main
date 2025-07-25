f12eb79ed6f95e7c19741f868a0ceb999e06dd1a
me-Sushil <girbahadurbk123@gmail.com>
2025-07-25 21:27:58 +0545

    call all solved

--- tests/7-testfirst-review/this-keyword-context/00-call-all/call-all.js
+++ tests/7-testfirst-review/this-keyword-context/00-call-all/call-all.js
@@ -1,1 +1,8 @@
 /* eslint-disable no-unused-vars */
+function callAll(obj, fnArr) {
+  let resultArray = [];
+  for (let fn of fnArr) {
+    resultArray.push(fn.call(obj));
+  }
+  return resultArray;
+}
