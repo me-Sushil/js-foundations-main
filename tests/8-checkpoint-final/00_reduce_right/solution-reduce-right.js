207e06bd1303ffeb0198d1cfe98d9c9389be58b8
me-Sushil <girbahadurbk123@gmail.com>
2025-07-25 22:12:41 +0545

    solution reduce right solved

--- tests/8-checkpoint-final/00_reduce_right/solution-reduce-right.js
+++ tests/8-checkpoint-final/00_reduce_right/solution-reduce-right.js
@@ -1,1 +1,9 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function reduceRight(arr, iniVal, func){
+    let accumulator = iniVal;
+    
+    for(let i= arr.length -1; i>=0; i--){
+        accumulator = func(accumulator, arr[i]);
+    }
+    return accumulator;
+}
