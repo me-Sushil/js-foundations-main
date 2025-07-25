da15b661ca20de066c7044be9b32eb0a53e63e1c
me-Sushil <girbahadurbk123@gmail.com>
2025-07-25 22:42:25 +0545

    alternet solution solved

--- tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
+++ tests/8-checkpoint-final/01_alternate_and_twice/solution-alternate-and-twice.js
@@ -1,1 +1,18 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function alternate(fn){
+    let toggle = true;     //runNextTime
+    return function(){
+        if(toggle){
+            fn();
+        }
+        toggle = !toggle;
+    }
+}
+function twice(callback){
+    let count = 0;
+    while(count < 2){
+        count++;
+        callback();
+    }
+
+}
