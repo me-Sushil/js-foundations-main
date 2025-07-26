b93d5a9394a6260db2efa5f3693199008ae14562
me-Sushil <girbahadurbk123@gmail.com>
2025-07-26 13:31:04 +0545

    key adder solved

--- tests/8-checkpoint-final/04_key_adder/solution-key-adder.js
+++ tests/8-checkpoint-final/04_key_adder/solution-key-adder.js
@@ -1,1 +1,10 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function keyAdder() {
+  let sum = 0;
+  for (let key in this) {
+    if (typeof this[key] === "number" && this.hasOwnProperty(key)) {
+      sum += this[key];
+    }
+  }
+  return sum;
+}
