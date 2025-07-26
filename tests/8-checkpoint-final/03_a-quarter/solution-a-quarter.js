49aaa23f78a83fd0348abb35389c03da71ba9fe8
me-Sushil <girbahadurbk123@gmail.com>
2025-07-26 13:07:51 +0545

    solution a quarter solved

--- tests/8-checkpoint-final/03_a-quarter/solution-a-quarter.js
+++ tests/8-checkpoint-final/03_a-quarter/solution-a-quarter.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+function aQuarter(func) {
+  let count = 0;
+  return function (...args) {
+    count++;
+    if (count % 4 === 0) {
+      return func(...args);
+    } else {
+      return `something went wrong :/`;
+    }
+  };
+}
