9295ebb9663d2529d0bb02f54c56349ac48b6eb4
me-Sushil <girbahadurbk123@gmail.com>
2025-07-26 15:07:31 +0545

    digit class solved

--- tests/8-checkpoint-final/05_character/solution-character.js
+++ tests/8-checkpoint-final/05_character/solution-character.js
@@ -1,30 +1,47 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
-class Character{
-    constructor(character, fontName, fontSize){
-        this.character = character;
-        this.fontName = fontName;
-        this.fontSize = fontSize;
-    }
+class Character {
+  constructor(character, fontName, fontSize) {
+    this.character = character;
+    this.fontName = fontName;
+    this.fontSize = fontSize;
+  }
 
-    drawCharacter(){
-        return this.character;
-    }
+  drawCharacter() {
+    return this.character;
+  }
 }
 
-class Letter extends Character{
-    constructor(character, fontName, fontSize, charCase){
-        super(character, fontName, fontSize);
-        this.charCase = charCase;
+class Letter extends Character {
+  constructor(character, fontName, fontSize, charCase) {
+    super(character, fontName, fontSize);
+    this.charCase = charCase;
+  }
+  toLowerCase() {
+    this.charCase = "lowerCase";
+    this.character = this.character.toLowerCase();
+  }
+  toUpperCase() {
+    this.charCase = "upperCase";
+    this.character = this.character.toUpperCase();
+  }
+  getCase() {
+    return this.charCase;
+  }
+}
+
+class Digit extends Character {
+  constructor(character, fontName, fontSize) {
+    super(character, fontName, fontSize);
+    this.isDigit = typeof character === 'number';
+    
+  }
+  integerValue() {
+    let parse = parseInt(this.character, 10);
+    if (!isNaN(parse)) {
+      this.isDigit = true;
+      this.character = parse;
+    } else {
+      this.isDigit = false;
     }
-    toLowerCase(){
-            this.charCase = 'lowerCase';
-            this.character = this.character.toLowerCase();
-    }
-    toUpperCase(){
-        this.charCase = 'upperCase';
-        this.character = this.character.toUpperCase();
-    }
-    getCase(){
-       return this.charCase;
-    }
+  }
 }
