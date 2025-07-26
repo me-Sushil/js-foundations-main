36d65de779360da1aab550817d36c0e6e6d171f0
me-Sushil <girbahadurbk123@gmail.com>
2025-07-26 12:07:52 +0545

    vending mavhine solved

--- tests/8-checkpoint-final/02_vending-machine/solution-vending-machine.js
+++ tests/8-checkpoint-final/02_vending-machine/solution-vending-machine.js
@@ -1,1 +1,30 @@
 /* eslint-disable no-unused-vars */
+class VM {
+  constructor(inventory) {
+    this.inventory = inventory;
+  }
+  sale(itemID) {
+    const item = this.inventory[itemID];
+    if (item && item.stock > 0) {
+      item.stock -= 1;
+      return `1 ${item.name} - Thank you and come again!`;
+    } else {
+      return "Item not available";
+    }
+  }
+  stockTotal() {
+    const stocktotal = Object.values(this.inventory).reduce(
+      (acc, currentVal) => {
+        return acc + currentVal.stock;
+      },
+      0
+    );
+    if (stocktotal === 0) {
+      return "Out of Stock";
+    } else if (stocktotal === 1) {
+      return "1 item";
+    } else {
+      return `${stocktotal} item(s)`;
+    }
+  }
+}
