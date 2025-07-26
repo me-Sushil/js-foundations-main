3e3203e508273ec2a42522b38ca852c8cdd31964
me-Sushil <girbahadurbk123@gmail.com>
2025-07-26 13:50:41 +0545

    Character class soolved

--- tests/8-checkpoint-final/05_character/solution-character.js
+++ tests/8-checkpoint-final/05_character/solution-character.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-unused-vars, no-prototype-builtins */
+class Character{
+    constructor(character, fontName, fontSize){
+        this.character = character;
+        this.fontName = fontName;
+        this.fontSize = fontSize;
+    }
+
+    drawCharacter(){
+        return this.character;
+    }
+}
